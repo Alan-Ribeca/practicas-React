471f465e278025596d9af8193cba471b9145a7bd
Alan Ribeca <alanribeca2003@gmail.com>
2024-03-21 16:19:12 -0300

    segundo proyecto terminado

--- calculadora de propina/calculadoraPropina/src/App.tsx
+++ calculadora de propina/calculadoraPropina/src/App.tsx
@@ -1,11 +1,13 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import MenuItem from "./components/MenuItem";
+import OrdenContents from "./components/OrdenContets";
+import OrderTotals from "./components/OrderTotals";
+import TipPercentageForm from "./components/TipPercentageForm";
 import { menuItems } from "./data/db";
 import useOrder from "./hooks/useOrder";
 
 function App() {
- 
-  const { addItem} = useOrder()
+  const { order, addItem, removeItem, tip, setTip, placeOrder } = useOrder();
 
   return (
     <>
@@ -26,8 +28,18 @@ function App() {
           </div>
         </div>
 
-        <div>
-          <h2>Consumo</h2>
+        <div className=" border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
+          {order.length ?(
+              <>
+                <OrdenContents order={order} removeItem={removeItem} />
+
+                <TipPercentageForm setTip={setTip} tip={tip} />
+
+                <OrderTotals order={order} tip={tip} placeOrder={placeOrder} />
+              </>
+            ) : (
+              <p className="text-center">La orden esta vacia</p>
+            )}
         </div>
       </main>
     </>
--- calculadora de propina/calculadoraPropina/src/hooks/useOrder.ts
+++ calculadora de propina/calculadoraPropina/src/hooks/useOrder.ts
@@ -4,13 +4,33 @@ import type { MenuItem, OrdenItem } from "../types";
 
 export default function useOrder() {
   const [order, setOrder] = useState<OrdenItem[]>([]);
+  const [tip, setTip] = useState(0);
 
   const addItem = (item: MenuItem) => {
-    
-    const newItem = {...item, quantity: 1}
-    setOrder([...order, newItem])
+    const itemExist = order.find((ordenItem) => ordenItem.id === item.id);
 
+    if (itemExist) {
+      const updateOrden = order.map((ordenItem) =>
+        ordenItem.id === item.id
+          ? { ...ordenItem, quantity: ordenItem.quantity + 1 }
+          : ordenItem
+      );
+      setOrder(updateOrden);
+    } else {
+      const newItem = { ...item, quantity: 1 };
+      setOrder([...order, newItem]);
+    }
+  };
+
+  const removeItem = (id: MenuItem["id"]) => {
+    setOrder(order.filter((item) => item.id !== id));
+  };
+
+
+  const placeOrder = () => {
+    setOrder([])
+    setTip(0)
   }
 
-  return {addItem}
+  return { order, tip, setTip, addItem, removeItem, placeOrder};
 }
