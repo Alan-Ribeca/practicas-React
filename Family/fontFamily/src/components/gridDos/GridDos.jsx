a87d88edb10f2f9b6f8d1f1fb743e0066a7a7b43
Alan Ribeca <alanribeca2003@gmail.com>
2024-04-07 09:08:48 -0300

    terminando el grid Dos

--- Family/fontFamily/src/components/gridDos/GridDos.jsx
+++ Family/fontFamily/src/components/gridDos/GridDos.jsx
@@ -3,68 +3,70 @@ import "./gridDos.scss";
 export const GridDos = () => {
   return (
     <>
-      <table className="gridDos">
-        <p className="calendario">Calendario</p>
-        <p className="mes">Abril</p>
-        <hr className="brCalendarioTop" />
-        <thead className="thead">
-          <tr className="tr">
-            <th className="th">Lu</th>
-            <th className="th">Ma</th>
-            <th className="th">Mi</th>
-            <th className="th">Ju</th>
-            <th className="th">Vi</th>
-            <th className="th">Sá</th>
-            <th className="th">Do</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr className="tr">
-            <td className="th">1</td>
-            <td className="th">2</td>
-            <td className="th">3</td>
-            <td className="th">4</td>
-            <td className="th">5</td>
-            <td className="th">6</td>
-            <td className="th">7</td>
-          </tr>
-          <tr className="tr">
-            <td className="th">8</td>
-            <td className="th">9</td>
-            <td className="th">10</td>
-            <td className="th">11</td>
-            <td className="th">12</td>
-            <td className="th">13</td>
-            <td className="th">14</td>
-          </tr>
-          <tr className="tr">
-            <td className="th">15</td>
-            <td className="th">16</td>
-            <td className="th">17</td>
-            <td className="th">18</td>
-            <td className="th">19</td>
-            <td className="th">20</td>
-            <td className="th">21</td>
-          </tr>
-          <tr className="tr">
-            <td className="th">22</td>
-            <td className="th">23</td>
-            <td className="th">24</td>
-            <td className="th">25</td>
-            <td className="th">26</td>
-            <td className="th">27</td>
-            <td className="th">28</td>
-          </tr>
-          <tr className="tr">
-            <td className="th">29</td>
-            <td className="th">30</td>
-            <td className="th"></td>
-            <td className="th"></td>
-          </tr>
-          <hr className="brCalendarioButtom" />
-        </tbody>
-        <button className="btnCalendario">Ver mas</button>
-      </table>
+      <div className="containerGridTable">
+        <table className="gridDos">
+          <p className="calendario">Calendario</p>
+          <p className="mes">Abril</p>
+          <hr className="brCalendarioTop" />
+          <thead className="thead">
+            <tr className="tr">
+              <th className="th">Lu</th>
+              <th className="th">Ma</th>
+              <th className="th">Mi</th>
+              <th className="th">Ju</th>
+              <th className="th">Vi</th>
+              <th className="th">Sá</th>
+              <th className="th">Do</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="tr">
+              <td className="th">1</td>
+              <td className="th">2</td>
+              <td className="th">3</td>
+              <td className="th">4</td>
+              <td className="th">5</td>
+              <td className="th">6</td>
+              <td className="th">7</td>
+            </tr>
+            <tr className="tr">
+              <td className="th">8</td>
+              <td className="th">9</td>
+              <td className="th">10</td>
+              <td className="th">11</td>
+              <td className="th">12</td>
+              <td className="th">13</td>
+              <td className="th">14</td>
+            </tr>
+            <tr className="tr">
+              <td className="th">15</td>
+              <td className="th">16</td>
+              <td className="th">17</td>
+              <td className="th">18</td>
+              <td className="th">19</td>
+              <td className="th">20</td>
+              <td className="th">21</td>
+            </tr>
+            <tr className="tr">
+              <td className="th">22</td>
+              <td className="th">23</td>
+              <td className="th">24</td>
+              <td className="th">25</td>
+              <td className="th">26</td>
+              <td className="th">27</td>
+              <td className="th">28</td>
+            </tr>
+            <tr className="tr">
+              <td className="th">29</td>
+              <td className="th">30</td>
+              <td className="th"></td>
+              <td className="th"></td>
+            </tr>
+            <hr className="brCalendarioButtom" />
+          </tbody>
+          <button className="btnCalendario">Ver mas</button>
+        </table>
+      </div>
     </>
   );
 };
