61eb072548c5acc562fbff6d728351d87b4aca02
Alan Ribeca <alanribeca2003@gmail.com>
2024-04-11 21:00:50 -0300

    terminando el grid cuatro

--- Family/fontFamily/src/components/gridCuatro/GridCuatro.jsx
+++ Family/fontFamily/src/components/gridCuatro/GridCuatro.jsx
@@ -14,17 +14,33 @@ export const GridCuatro = () => {
     <>
       <div className="containerGridCuatro">
         <h5 className="detalles">Detalles</h5>
-        <hr  className="hrDetalles"/>
+        <hr className="hrDetalles" />
         <p className="nombreFuente">
           Fuente: {selectedFont ? selectedFont : "Arial"}
         </p>
         <p className="nombreFuente">Total de fuentes: 1618</p>
         <p className="nombreFuente">
-          Numero de la fuente: {indexActual ? indexActual : 1}
+          Numero de la fuente: {indexActual ? indexActual : 0}
+        </p>
+        <p className="nombreFuente">
+          <a
+            href={`https://fonts.google.com/specimen/${selectedFont
+              .split(" ")
+              .join("+")}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="fuenteA"
+          >
+            Usar fuente...
+          </a>
         </p>
         <div className="containerBoton">
-          <button className="btnAnterior" onClick={handleClickAnterior}>Anterior</button>
-          <button className="btnSiguiente" onClick={handleClickSiguiente}>Siguiente</button>
+          <button className="btnAnterior" onClick={handleClickAnterior}>
+            Anterior
+          </button>
+          <button className="btnSiguiente" onClick={handleClickSiguiente}>
+            Siguiente
+          </button>
         </div>
       </div>
     </>
--- Family/fontFamily/src/components/navbar/Header.jsx
+++ Family/fontFamily/src/components/navbar/Header.jsx
@@ -1,10 +1,12 @@
+import  img  from "../../logo/logoListo.png"
 import "./navbar.scss";
+
 
 export const Header = () => {
   return (
     <header className="header">
       <nav className="navbar">
-        <p>logo</p>
+        <img src={img} alt="logo" className="logo"/>
         <ul className="ulNav">
           <li className="liNav">Crear</li>
           <li className="liNav">Navagar</li>
